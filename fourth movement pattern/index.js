2ffb8efee5f39803a0cecd2c60f17b8a9cbe3c5f
julian <juliandecastelli@gmail.com>
2022-09-21 12:08:41 -0300

    add fourth  movement pattern

---
+++ fourth movement pattern/index.js
@@ -0,0 +1,70 @@
+/** @type {HTMLCanvasElement} */
+const canvas = document.getElementById("canvas1");
+ctx = canvas.getContext("2d");
+const CANVAS_WIDTH = (canvas.width = 500);
+const CANVAS_HEIGHT = (canvas.height = 1000);
+const numberOfEnemies = 50;
+const enemyArray = [];
+
+let gameFrame = 0;
+
+class Enemy {
+  constructor() {
+    this.Image = new Image();
+    this.Image.src = "/enemies/enemy4.png";
+    this.speed = Math.random() * 4 + 1;
+    this.spriteWidht = 213;
+    this.spriteHeight = 213;
+    this.width = this.spriteWidht / 2.5;
+    this.height = this.spriteHeight / 2.5;
+    this.x = Math.random() * (CANVAS_WIDTH - this.width);
+    this.y = Math.random() * (CANVAS_HEIGHT - this.height);
+    this.newX = Math.random() * (CANVAS_WIDTH - this.width);
+    this.newY = Math.random() * (CANVAS_HEIGHT - this.height);
+    this.frame = 0;
+    this.flapSpeed = Math.floor(Math.random() * 3 + 1);
+    this.interval = Math.floor(Math.random() * 200 + 50);
+  }
+  update() {
+    if (gameFrame % this.interval === 0) {
+      this.newX = Math.random() * (CANVAS_WIDTH - this.width);
+      this.newY = Math.random() * (CANVAS_HEIGHT - this.height);
+    }
+    let dx = this.x - this.newX;
+    let dy = this.y - this.newY;
+    if (this.x + this.width < 0) this.x = CANVAS_WIDTH;
+    if (gameFrame % this.flapSpeed === 0) {
+      this.frame > 4 ? (this.frame = 0) : this.frame++;
+    }
+    this.x -= dx / 70;
+    this.y -= dy / 70;
+  }
+  draw() {
+    ctx.drawImage(
+      this.Image,
+      this.frame * this.spriteWidht,
+      0,
+      this.spriteWidht,
+      this.spriteHeight,
+      this.x,
+      this.y,
+      this.width,
+      this.height
+    );
+  }
+}
+
+for (let i = 0; i < numberOfEnemies; i++) {
+  enemyArray.push(new Enemy());
+}
+
+function animate() {
+  ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  enemyArray.forEach((enemy) => {
+    enemy.update();
+    enemy.draw();
+  });
+  gameFrame++;
+  requestAnimationFrame(animate);
+}
+animate();

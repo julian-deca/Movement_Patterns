fe3c7c386f37e9d6a21ae4143fcdb85e928b35a8
julian <juliandecastelli@gmail.com>
2022-09-21 11:40:58 -0300

    add second movement pattern

--- second movement pattern/index.js
+++ second movement pattern/index.js
@@ -11,20 +11,25 @@ let gameFrame = 0;
 class Enemy {
   constructor() {
     this.Image = new Image();
-    this.Image.src = "/enemies/enemy1.png";
-    // this.speed = Math.random() * 4 - 2;
-    this.spriteWidht = 293;
-    this.spriteHeight = 155;
+    this.Image.src = "/enemies/enemy2.png";
+    this.speed = Math.random() * 4 + 1;
+    this.spriteWidht = 266;
+    this.spriteHeight = 188;
     this.width = this.spriteWidht / 2.5;
     this.height = this.spriteHeight / 2.5;
     this.x = Math.random() * (CANVAS_WIDTH - this.width);
     this.y = Math.random() * (CANVAS_HEIGHT - this.height);
     this.frame = 0;
     this.flapSpeed = Math.floor(Math.random() * 3 + 1);
+    this.angle = Math.random() * 2;
+    this.angleSpeed = Math.random() * 0.2;
+    this.curve = Math.random() * 3 + 3;
   }
   update() {
-    this.x += Math.random() * 5 - 2.5;
-    this.y += Math.random() * 5 - 2.5;
+    this.x -= this.speed;
+    this.y += this.curve * Math.sin(this.angle);
+    this.angle += this.angleSpeed;
+    if (this.x + this.width < 0) this.x = CANVAS_WIDTH;
     if (gameFrame % this.flapSpeed === 0) {
       this.frame > 4 ? (this.frame = 0) : this.frame++;
     }
